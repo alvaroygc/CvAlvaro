d9f27123c07663f09ebf627deb3b058ed61bd075
Alvaroygc <alvarogudiel@gmail.com>
2021-05-04 21:03:26 -0600

    Cambios de Experencia salto linea

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,15 +3,14 @@ import H2Styled from '../styled/H2Styled';
 import H3Styled from '../styled/H3Styled';
 import PStyled from '../styled/PStyled';
 
-
 const Experience = props => (
     <div className='Experience'>
     <H2Styled name='Experiencia' />  
         <div className='Experience-container'>
         {props.data.map((exp, index) => (
                 <div className='Experience-item' key={`Exp-${index}`}>                             
-                <H3Styled><br/>
-                {exp.jobTitle}  <br/>
+                <H3Styled> <br/>
+                {exp.jobTitle} <br/>
                 {exp.company} <br/>
                 <span>{exp.startDate} - {exp.endDate}</span>
                 </H3Styled>
