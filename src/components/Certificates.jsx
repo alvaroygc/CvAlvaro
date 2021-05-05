6a35a7c11b029f0ca0339398be5d95f5e89997c9
Alvaroygc <alvarogudiel@gmail.com>
2021-05-04 21:18:22 -0600

    Cambio Certificado

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -10,8 +10,10 @@ const Certificates = props =>(
         <div className='Certificates-container'>
         {props.data.map((cer, index) => (
                 <div className='Certificate-item' key={`Cer-${index}`}>                             
-                <H3Styled>{cer.name}@ {cer.institution} 
-                <span>{cer.date} - {cer.endDate}</span>
+                <H3Styled> <br/>
+                {cer.name} <br/>
+                {cer.institution} <br/> 
+                <span>{cer.date}</span>
                 </H3Styled>
                 <PStyled name= {cer.description}/>
             </div>        
